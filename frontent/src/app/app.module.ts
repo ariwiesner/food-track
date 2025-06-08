eed05713b4552f6f53aff50e837ce256d6ea58bf
ariwiesner <ari.w.05122@gmail.com>
2025-06-08 15:57:01 +0300

    created navbar component

--- frontent/src/app/app.module.ts
+++ frontent/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent,
-    HomePageComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule
